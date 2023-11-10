da20b253a49a79bbc6c2776b59e26f48863ef3b9
DESKTOP-JOOKDG6\profesor <csi23-amoliaz@colegioaltair.net>
2023-11-10 12:03:59 +0100

    no message

---
+++ src/edu/ejercicio4ED/ejercicio4ED.java
@@ -0,0 +1,34 @@
+package edu.ejercicio4ED;
+
+import java.util.Scanner;
+/*
+ * clase principal
+ * @author amd-101123
+ */
+public class ejercicio4ED 
+{
+	/*
+	 * metodo principal
+	 * @author amd-101123
+	 */
+	public static void main(String[] args) 
+	{
+		Scanner scan = new Scanner(System.in);
+		System.out.println("escribe un double");
+		double a = scan.nextDouble();	
+		System.out.println("escribe un double");
+		double b = scan.nextDouble();	
+		System.out.println("escribe un double");
+		double c = scan.nextDouble();	
+		if()
+			
+			
+			
+	
+		
+		
+	}
+}
+	
+
+
