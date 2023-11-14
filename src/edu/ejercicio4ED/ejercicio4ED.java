d17f069f47bb943abeff1cc4a015371eb1dd40b0
DESKTOP-JOOKDG6\profesor <csi23-amoliaz@colegioaltair.net>
2023-11-14 11:42:56 +0100

    no message

--- src/edu/ejercicio4ED/ejercicio4ED.java
+++ src/edu/ejercicio4ED/ejercicio4ED.java
@@ -15,18 +15,43 @@ public class ejercicio4ED
 	{
 		Scanner scan = new Scanner(System.in);
 		System.out.println("escribe un double");
-		double a = scan.nextDouble();	
+		double i = scan.nextDouble();	
 		System.out.println("escribe un double");
-		double b = scan.nextDouble();	
+		double j = scan.nextDouble();	
 		System.out.println("escribe un double");
-		double c = scan.nextDouble();	
-		if()
-			
-			
-			
-	
-		
-		
+		double k = scan.nextDouble();	
+		if (i>=j & i>=k)
+        {
+            if (j >= k)
+            {
+            	System.out.println("Orden: " + i + j + k);
+            }
+            else
+            {
+            	System.out.println("Orden: " + i + k + j);
+            }
+        }else if (j>=i & j>=k)
+        {
+            if (i >= k)
+            {
+            	System.out.println("Orden: " + j + i + k);
+            }
+            else
+            {
+            	System.out.println("Orden: " + j + k + i);
+            }
+        } else if (k>=i & k>=j)
+        {
+            if (i >= j)
+            {
+            	System.out.println("Orden: " + k + i + j);
+            }
+            else
+            {
+            	System.out.println("Orden: " +k + j + i);
+            }
+        }
+       	
 	}
 }
 	
